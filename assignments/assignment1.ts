ed4148aa40c5a1adac27a6d9aa3ae3f428d89b49
Anurag Singh <anurags655@gmail.com>
2026-02-10 21:33:57 +0530

    assignment_1

---
+++ assignments/assignment1.ts
@@ -0,0 +1,80 @@
+// 1.Temperature of a city in degrees Celsius: 25.5;
+let temperature : number = 25.5;
+console.log(temperature);
+
+//2.Whether a customer has placed an order: true or false
+let orderPlaced : boolean = true;
+console.log(orderPlaced);
+
+//3.Person's phone number: "123-456-7890"
+const PHONE_NUMBER : string = "123-456-7890";
+console.log(PHONE_NUMBER);
+
+// 4.Amount of money in a customer's bank account: 1000.50
+let money : number = 1000.50;
+console.log(money);
+
+// 5.Person's email address: "john.doe@example.com"
+const EMAIL : string = "john.doe@example.com";
+console.log(EMAIL);
+
+// 6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinates : number[] = [37.7749, -122.4194];
+console.log(coordinates);
+
+// 7. Person's marital status: true or false
+let maritalStatus : boolean = false;
+console.log(maritalStatus);
+
+// 8.Person's occupation: "Software Engineer"
+let occupation : string = "Software Engineer";
+console.log(occupation);
+
+// 9.Person's favourite colour: "Blue"
+let favColour: string = "Blue";
+console.log(favColour);
+
+// 10.Current year: 2023
+let currentYear : number = 2023;
+console.log(currentYear);
+
+// 11.Number of followers on a social media platform: 1,000,000
+let numberOfFollowers: number = 1000000;
+console.log(numberOfFollowers);
+
+// 12.Rating of a movie: 7.5
+let rating : number = 7.5;
+console.log(rating);
+
+// 13.Person's blood type: 'A'
+const bloodType : string = 'A';
+console.log(bloodType);
+
+// 14.Title of a book: "To Kill a Mockingbird"
+let titleOfBook : string = "To Kill a Mockingbird";
+console.log(titleOfBook);
+
+// 15.Number of employees in a company: 500
+let employeesCount : number = 500;
+console.log(employeesCount);
+
+// 16.Time of an event: 2:30 PM
+let time : string = '2:30 PM';
+console.log(time);
+
+// 17.Name of a country: "United States"
+let countryName : string = "United States";
+console.log(countryName);
+
+// 18.Person's eye color: "Brown"
+let eyeColor : string = "Brown";
+console.log(eyeColor);
+
+// 19.Person's birthplace: "New York City"
+let birthPlace : string = "New York City";
+console.log(birthPlace);
+
+// 20.Distance between two cities: 200.5
+const DISTANCE : number = 200.5;
+console.log(DISTANCE);
+
---
+++ javascript-training/hello-world.js
@@ -0,0 +1,8 @@
+// there are 3 types of print statements in JS
+console.log("Hello, World!");
+
+console.info("this is an informational message");
+
+console.warn("This is a warning message");
+
+console.error("This is an error");
